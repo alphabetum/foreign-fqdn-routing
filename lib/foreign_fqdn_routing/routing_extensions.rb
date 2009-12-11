d89b2b8e77ef1d23c9c420d8b83f98860e3ab61d
William Melody <wmelody@gmail.com>
2009-12-10 23:09:38 -0600

    formatting

--- lib/foreign_fqdn_routing/routing_extensions.rb
+++ lib/foreign_fqdn_routing/routing_extensions.rb
@@ -1,8 +1,17 @@
 module ForeignFQDNRouting
   module RouteExtensions
 
-    TESTABLE_REQUEST_METHODS = [:subdomain, :domain, :method, :port, :remote_ip, 
-                                :content_type, :accepts, :request_uri, :protocol]
+    TESTABLE_REQUEST_METHODS = [
+      :subdomain,
+      :domain,
+      :method,
+      :port,
+      :remote_ip, 
+      :content_type,
+      :accepts,
+      :request_uri,
+      :protocol
+    ]
     
     def self.included(base)
       base.alias_method_chain :recognition_conditions, :foreign_domain
@@ -10,10 +19,10 @@ module ForeignFQDNRouting
 
     def recognition_conditions_with_foreign_domain      
       result = recognition_conditions_without_foreign_domain
-      result << "ForeignFQDNRouting.foreign_domain?(env[:host])" if conditions[:foreign_domain] == true
-      result << "!ForeignFQDNRouting.foreign_domain?(env[:host])" if conditions[:foreign_domain] == false
-      result << "ForeignFQDNRouting.foreign_fqdn?(env[:host])" if conditions[:foreign_fqdn] == true
-      result << "!ForeignFQDNRouting.foreign_fqdn?(env[:host])" if conditions[:foreign_fqdn] == false
+      result << "ForeignFQDNRouting.foreign_domain?(env[:host])"  if conditions[:foreign_domain]  == true
+      result << "!ForeignFQDNRouting.foreign_domain?(env[:host])" if conditions[:foreign_domain]  == false
+      result << "ForeignFQDNRouting.foreign_fqdn?(env[:host])"    if conditions[:foreign_fqdn]    == true
+      result << "!ForeignFQDNRouting.foreign_fqdn?(env[:host])"   if conditions[:foreign_fqdn]    == false
       
       conditions.each do |method, value|
         if TESTABLE_REQUEST_METHODS.include? method
@@ -24,12 +33,10 @@ module ForeignFQDNRouting
           end
         else
         end
-      end
-      
-      
+      end # conditions
       result
-    end
-  end
+    end # def
+  end # RouteExtensions
 
   module RouteSetExtensions
     def self.included(base)
@@ -38,16 +45,16 @@ module ForeignFQDNRouting
 
     def extract_request_environment_with_foreign_domain(request)
       extract_request_environment_without_foreign_domain(request).merge({ 
-        :host => request.host,
-        :method => request.method,
-        :subdomain => request.subdomains.first.to_s, 
-        :domain => request.domain, 
-        :port => request.port, 
-        :remote_ip => request.remote_ip, 
+        :host         => request.host,
+        :method       => request.method,
+        :subdomain    => request.subdomains.first.to_s, 
+        :domain       => request.domain, 
+        :port         => request.port, 
+        :remote_ip    => request.remote_ip, 
         :content_type => request.content_type, 
-        :accepts => request.accepts.map(&:to_s).join(','), 
-        :request_uri => request.request_uri, 
-        :protocol => request.protocol
+        :accepts      => request.accepts.map(&:to_s).join(','), 
+        :request_uri  => request.request_uri, 
+        :protocol     => request.protocol
         })
     end
   end
